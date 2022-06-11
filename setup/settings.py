60ee2d20fcd8ea6d1dab67c57cc333b5c57d9648
AntonioMateus95 <antoniocmsilva95@gmail.com>
2022-06-11 17:58:52 -0300

    Adding dependencies to use postgres

--- setup/settings.py
+++ setup/settings.py
@@ -67,8 +67,11 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432')
     }
 }
 
