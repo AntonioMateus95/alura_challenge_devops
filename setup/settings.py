2c2a7e8d5cb8e1ac6cd31cde525d4721660ca9c3
AntonioMateus95 <antoniocmsilva95@gmail.com>
2022-06-15 22:30:00 -0300

    Fixing postgresql database access

--- setup/settings.py
+++ setup/settings.py
@@ -68,7 +68,8 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_USER'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT', '5432')
