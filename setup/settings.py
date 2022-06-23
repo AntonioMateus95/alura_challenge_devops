dba551094f027523c5a11b16265bd262a58175f9
AntonioMateus95 <antoniocmsilva95@gmail.com>
2022-06-22 23:55:17 -0300

    Fixing manage.py

--- setup/settings.py
+++ setup/settings.py
@@ -8,13 +8,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '_&#6!!_&e!b*2y*woaoqy!g=z+en8)%3_g9(z&@cm4_&h#7wis'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
-
+ALLOWED_HOSTS = (os.environ.get('ALLOWED_HOSTS') and os.environ.get('ALLOWED_HOSTS').split(',')) or []
 
 # Application definition
 
